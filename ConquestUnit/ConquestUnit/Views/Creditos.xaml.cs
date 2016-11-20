64af3f4968250c6d8187d3e85611ba0fec0daca5
RoyTR <roytazarojas@gmail.com>
2016-11-19 20:13:40 -0500

    Ocultado boton de como jugar y Mejora del cartel de Victoria

--- ConquestUnit/ConquestUnit/Views/Creditos.xaml.cs
+++ ConquestUnit/ConquestUnit/Views/Creditos.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.Graphics.Display;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +15,11 @@ namespace ConquestUnit.Views
         public Creditos()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Portrait;
         }
 
         private void btnAtras_Tapped(object sender, TappedRoutedEventArgs e)
